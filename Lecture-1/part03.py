88ace3bf7c7ca5937641bbd330e6e8c80ad7462a
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2024-05-31 05:26:07 +0600

    Add files via upload

---
+++ Lecture-1/part03.py
@@ -0,0 +1,24 @@
+# Ask user for their name
+
+# def hello(to="world"):
+#     print("hello", to)
+
+# hello()
+
+# name = input("What is your name? ")
+# hello(name)
+
+
+# Scope
+
+
+def main():
+    name = input("What is your name? ")
+    hello(name)
+
+
+def hello(to="world"):
+    print("hello", to)
+
+main()
+
