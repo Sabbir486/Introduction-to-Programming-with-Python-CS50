c390902606d40f803ecf1f36d2b1d41ded847dbb
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2024-06-08 06:39:15 +0600

    Add files via upload

---
+++ Lecture-3/part01.py
@@ -0,0 +1,31 @@
+# Loops
+
+# While Loop
+
+# i = 1
+# while i <= 3:
+#     print("meow")
+#     i += 1
+
+
+# For Loop
+
+# for _ in range(0,3) :
+#     print("meow")
+
+
+# Special Process
+
+# print("meow \n" * 3, end="")
+
+
+# Asking from User
+
+while True:
+    n = int(input("What's n? "))
+
+    if n>0:
+        break
+
+for _ in range(n) :
+    print("meow")
